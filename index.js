e872009731cead679cb7d54ada1bc9873658d40d
Jerryomwesh <jerryomwesh@gmail.com>
2025-06-28 01:03:24 +0300

    DOM modifying element

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Remove the <main> element with id 'main'
+document.getElementById('main').remove();
+
+// Create a new <h1> element and assign it to 'newHeader'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Jerry is the champion';
+
+document.body.append(newHeader);
+
+// Make newHeader globally accessible for tests
+window.newHeader = newHeader;
